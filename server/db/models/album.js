4d9b88a117a0453d39f2535b2d61790814c8cc66
yungtiec <tctammychu@gmail.com>
2016-07-06 14:08:34 -0400

    first commit

---
+++ server/db/models/album.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const db = require('../db');
+const DataTypes = db.Sequelize;
+const unique = require('./plugins/unique-through')
+
+module.exports = db.define('album', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set: function (val) {
+      this.setDataValue('name', val.trim());
+    }
+  },
+  cover: {
+    type: DataTypes.BLOB
+  },
+  coverType: {
+    type: DataTypes.STRING
+  },
+  artists: unique('artists').through('songs')
+}, {
+  defaultScope: {
+    attributes: { exclude: ['cover', 'coverType'] }
+  },
+  scopes: {
+    populated: () => ({ // function form lets us use to-be-defined models
+      include: [{
+        model: db.model('song'), // populated with artists due to song model
+      }]
+    })
+  }
+});
